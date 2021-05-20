154caa762ce0c67491e3f549a05c29b3c2220a98
Guru-Eco <abhimanyu768692@gmail.com>
2021-05-20 18:02:00 +0530

    left sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 import './App.css';
+import SideBar from "./Components/SideBar/SideBar";
 
 function App() {
   return (
     <div className="App">
- 
+      <SideBar />
     </div>
   );
 }
--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { SideBarContent } from "./SideBarContent";
+import { Link } from "react-router-dom";
+import "./SideBar.css";
+import "../../App.css";
 
 function SideBar() {
     return (
-        <div>
-            
+        <div className="sideBarContainer">
+            <div className="sideBarLinksContainer">
+                <Link className="flexBox sideBarContentLink" to="/">
+                    <SideBarContent src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" label="Darshan Naik" />
+                </Link>
+                <Link className="flexBox sideBarContentLink" to="/">
+                    <SideBarContent src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" label="Friends" />
+                </Link>
+                <Link className="flexBox sideBarContentLink" to="/">
+                    <SideBarContent src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" label="Messenger" />
+                </Link>
+                <Link className="flexBox sideBarContentLink" to="/">
+                    <SideBarContent src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" label="Photos" />
+                </Link>
+                <Link className="flexBox sideBarContentLink" to="/">
+                    <SideBarContent src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" label="Posts" />
+                </Link>
+                <Link className="flexBox sideBarContentLink" to="/">
+                    <SideBarContent src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" label="Favourites" />
+                </Link>
+            </div>
         </div>
     )
 }
