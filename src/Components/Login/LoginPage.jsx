00271e8539a8d9d1e370d4f6d36e3067266ee837
darshan <darshu8314@gmail.com>
2021-06-02 20:12:53 +0530

    new post card added

--- src/Components/Login/LoginPage.jsx
+++ src/Components/Login/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../App.css";
 import "../../Styles/Login/LoginPage.css"
 import LoginLogo from './LoginLogo';
@@ -6,14 +6,15 @@ import LoginForm from './LoginForm';
 import {ReactComponent as CloseIcon} from  "../../Icons/close.svg"
 import { login, signup } from '../../Firebase/authentication';
 import { database } from '../../Firebase/firebase';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loginFailure, loginRequest, loginSuccess, signUpFailure, signupRequest, signupSuccess } from '../../Redux/Auth/actions';
 
 const initFormSignup = {
     first_name :"",
     last_name : "",
     email : "",
-    password : ""
+    password : "",
+    gender: ""
 }
 const initFormLogin = {
     email : "",
@@ -23,16 +24,40 @@ function LoginPage(){
     const [isCreateClick, setIsCreateClick] = useState(false);
     const [signUpForm,setSignUpForm] = React.useState(initFormSignup)
     const [logInForm,setLogInForm] = React.useState(initFormLogin)
-    const {first_name , last_name,email, password} = signUpForm;
+    const {first_name , last_name,email, password, gender} = signUpForm;
+    const [dob, setDob] = useState("1, June, 1950");
+    const [day, setDay] = useState("1");
+    const [mon, setMon] = useState("June");
+    const [year, setYear] = useState("1950");
+    const {errorMessage, isError} = useSelector(store=>store.auth)
     const handleSignUpForm = (e)=>{
-            const {value,name} = e.target;
-            setSignUpForm({...signUpForm,[name]:value}) 
+        //console.log(e.target.value)
+        const {value,name} = e.target;
+        setSignUpForm({...signUpForm,[name]:value}) 
+        //console.log(signUpForm)
     }
 
     const handleLoginForm = (e)=>{
         const {value,name} = e.target;
         setLogInForm({...logInForm,[name]:value}) 
     }
+
+    const handleChangeDob = (e) => {
+        if(e.target.name == "date_of_birth:day"){
+            setDay(e.target.value);
+        } else if (e.target.name == "date_of_birth:mon"){
+            setMon(e.target.value);
+        } else if (e.target.name == "date_of_birth:year"){
+            setYear(e.target.value);
+        }
+        
+        //console.log(dob)
+    }
+
+    useEffect(()=>{
+        setDob(day+", "+mon+", "+year);
+        console.log(dob)
+    },[day, mon, year])
 
     const dispatch =useDispatch()
 
@@ -42,13 +67,12 @@ function LoginPage(){
         signup(email,password)
         .then(res=>{
            const {uid} = res.user
-           const payload = {uid,...signUpForm}
+           const payload = {uid,...signUpForm,dob}
            database.collection("users").add(payload).then((res)=>{
-               console.log(res.id)
-                dispatch(signupSuccess(payload))
+                dispatch(signupSuccess({docID :res.id,...payload}))
            })
         }).catch((err)=>{
-            dispatch(signUpFailure(err))
+            dispatch(signUpFailure(err.message))
         })
 
     }
@@ -77,7 +101,9 @@ function LoginPage(){
         setIsCreateClick(false);
     }
 
-    const years = new Array(60).fill(1950).map((el,i)=>el+i)
+    const years = new Array(60).fill(1950);
+    const days = new Array(31).fill(1);
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
         return  (<>
         <div>
@@ -96,8 +122,12 @@ function LoginPage(){
             <h2 className="signUpH2">Sign Up</h2>
             <p className="signUpP">It's quick and easy.</p>
             <hr className="signUpHr"/>
+            {/* <br /> */}
             <button className="signUpbuttonClose" onClick={handleCloseClick}><CloseIcon /></button>
-            <br />
+            {isError && <div className="errorHandlingContainerSignUp">
+                <p>{errorMessage}</p>
+            </div>
+            }
             <div className="nameSignUpContainer" >
                 <input type="text" placeholder="First name" value={first_name} name="first_name"  onChange={handleSignUpForm} />
                 <input type="text" placeholder="Surname" value={last_name}  name="last_name" onChange={handleSignUpForm}/>
@@ -106,151 +136,43 @@ function LoginPage(){
             <input className="numPassInput" type="password" value={password}  placeholder="New password" name="password" onChange={handleSignUpForm} />
             <div className="dobSignUpContainer">
                 <p>Date of birth</p>
-                <select class="first" name="date_of_birth:day" tabindex="7">
-                        {/* <option value="">
-                        <!-- -->
-                        </option> */}
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
-                    </select>
-                    <select name="date_of_birth:mon" tabindex="8">
-                        {/* <option value="">
-                        <!-- -->
-                        </option> */}
-                        <option value="1">Jan</option>
-                        <option value="2">Feb</option>
-                        <option value="3">Mar</option>
-                        <option value="4">Apr</option>
-                        <option value="5">May</option>
-                        <option value="6">Jun</option>
-                        <option value="7">Jul</option>
-                        <option value="8">Aug</option>
-                        <option value="9">Sep</option>
-                        <option value="10">Oct</option>
-                        <option value="11">Nov</option>
-                        <option value="12">Dec</option>
-                    </select>
-                    <select name="date_of_birth:year" tabindex="9">
-                        {/* <option value="">
-                        <!-- -->
-                        </option> */}
-                        <option value="1920">1920</option>
-                        <option value="1921">1921</option>
-                        <option value="1922">1922</option>
-                        <option value="1923">1923</option>
-                        <option value="1924">1924</option>
-                        <option value="1925">1925</option>
-                        <option value="1926">1926</option>
-                        <option value="1927">1927</option>
-                        <option value="1928">1928</option>
-                        <option value="1929">1929</option>
-                        <option value="1930">1930</option>
-                        <option value="1931">1931</option>
-                        <option value="1932">1932</option>
-                        <option value="1933">1933</option>
-                        <option value="1934">1934</option>
-                        <option value="1935">1935</option>
-                        <option value="1936">1936</option>
-                        <option value="1937">1937</option>
-                        <option value="1938">1938</option>
-                        <option value="1939">1939</option>
-                        <option value="1940">1940</option>
-                        <option value="1941">1941</option>
-                        <option value="1942">1942</option>
-                        <option value="1943">1943</option>
-                        <option value="1944">1944</option>
-                        <option value="1945">1945</option>
-                        <option value="1946">1946</option>
-                        <option value="1947">1947</option>
-                        <option value="1948">1948</option>
-                        <option value="1949">1949</option>
-                        <option value="1950">1950</option>
-                        <option value="1951">1951</option>
-                        <option value="1952">1952</option>
-                        <option value="1953">1953</option>
-                        <option value="1954">1954</option>
-                        <option value="1955">1955</option>
-                        <option value="1956">1956</option>
-                        <option value="1957">1957</option>
-                        <option value="1958">1958</option>
-                        <option value="1959">1959</option>
-                        <option value="1960">1960</option>
-                        <option value="1961">1961</option>
-                        <option value="1962">1962</option>
-                        <option value="1963">1963</option>
-                        <option value="1964">1964</option>
-                        <option value="1965">1965</option>
-                        <option value="1966">1966</option>
-                        <option value="1967">1967</option>
-                        <option value="1968">1968</option>
-                        <option value="1969">1969</option>
-                        <option value="1970">1970</option>
-                        <option value="1971">1971</option>
-                        <option value="1972">1972</option>
-                        <option value="1973">1973</option>
-                        <option value="1974">1974</option>
-                        <option value="1975">1975</option>
-                        <option value="1976">1976</option>
-                        <option value="1977">1977</option>
-                        <option value="1978">1978</option>
-                        <option value="1979">1979</option>
-                        <option value="1980">1980</option>
-                        <option value="1981">1981</option>
-                        <option value="1982">1982</option>
-                        <option value="1983">1983</option>
-                        <option value="1984">1984</option>
-                        <option value="1985">1985</option>
-                        <option value="1986">1986</option>
-                        <option value="1987">1987</option>
-                        <option value="1988">1988</option>
-                        <option value="1989">1989</option>
-                        <option value="1990">1990</option>
-                        <option value="1991">1991</option>
-                        <option value="1992">1992</option>
-                        <option value="1993">1993</option>
-                        <option value="1994">1994</option>
-                        <option value="1995">1995</option>
-                        <option value="1996">1996</option>
-                        <option value="1997">1997</option>
-                        <option value="1998">1998</option>
-                        <option value="1999">1999</option>
-                        <option value="2000">2000</option>
-                    </select>
-                </div>
-                <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={handleSingUp}>Sign-up</button>
-                </div>
+                <select onChange={(e)=>handleChangeDob(e)} className="first" name="date_of_birth:day" tabindex="7">
+                    {days.map((el,i)=>(
+                        <option key={el+i} value={el+i}>{el+i}</option>
+                    ))}
+                </select>
+                <select onChange={(e)=>handleChangeDob(e)} name="date_of_birth:mon" tabindex="8">
+                    {months.map((el)=>(
+                        <option key={el} value={el}>{el}</option>
+                    ))}
+                </select>
+                <select onChange={(e)=>handleChangeDob(e)} name="date_of_birth:year" tabindex="9">
+                    {years.map((el,i)=>(
+                        <option key={el+i} value={el+i}>{el+i}</option>
+                    ))}
+                </select>
+            </div>
+            <p className="genderPtag">Gender</p>
+            <div className="dobSignUpContainer flexBox">
                 
-        </div>)}
-        </>
+                <div>
+                    <input type="radio" onChange={(e)=>handleSignUpForm(e)} value="MALE" name="gender"/> 
+                    <p>Male</p>
+                </div>
+                <div>
+                    <input type="radio" onChange={(e)=>handleSignUpForm(e)} value="FEMALE" name="gender"/>
+                    <p>Female</p>
+                </div>
+                <div>
+                    <input type="radio" onChange={(e)=>handleSignUpForm(e)} value="OTHERS" name="gender"/>
+                    <p>Others</p>
+                </div>
+            </div>
+            <p className="termsSignUpContainer">By clicking Sign Up, you agree to our Terms, Data Policy and Cookie Policy. </p>
+            <button className="signUpButton" onClick={handleSingUp}>Sign-up</button>
+        </div>       
+    </div>)}
+    </>
 
     )
 }
