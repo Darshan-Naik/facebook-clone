bf5db1d8a7dbd71deffe4610e0396dea297f4cf5
Soumyadri Das <dadsoumyadri@gmail.com>
2021-06-03 18:47:03 +0530

    Loading state added into login and signUp model

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { WaveLoading } from "react-loadingg";
 
-function LoginForm({onClickCreate,password,email,handleLogin,handleLoginForm}){
-    
+function LoginForm({onClickCreate,password,email,handleLogin,handleLoginForm,isLoading}){
     return (
         <div className="loginFormMainDiv">
             <input type="text" value={email}  name="email" onChange={handleLoginForm} placeholder="Email address or phone number"/>
             <input type="password" value={password} onChange={handleLoginForm} name="password" placeholder="Password" />
-            <button onClick={handleLogin}>Log In</button>
+            <button disabled={isLoading} onClick={handleLogin}>{!isLoading?"Log In":<WaveLoading size="small" color="var(--primary-background)" style={{margin: "auto"}}/>}</button>
             <Link to="/">Forgotten password?</Link>
             <hr className="hrLoginForm" />
             <button onClick={onClickCreate}>Create New Account</button>
