bac0ef8e0d137250c88fe21834b1028aed2a1c41
Soumyadri Das <dadsoumyadri@gmail.com>
2021-06-03 22:17:38 +0530

    Story Container design modified

--- src/Components/Story/StoryContainer.jsx
+++ src/Components/Story/StoryContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../../Styles/Story/Story.css";
 import "../../App.css";
 import { useSelector } from 'react-redux';
@@ -6,9 +6,21 @@ import { useSelector } from 'react-redux';
 function StoryContainer(){
     const posts = useSelector(store=>store.posts.posts);
     console.log(posts)
+    // useEffect(()=>{
+    //     console.log("P",posts)
+    // },[posts])
+    var imagePosts = [];
+    for(var i=0; i<posts.length; i++){
+        if(posts[i].hasOwnProperty('image')){
+            imagePosts.push(posts[i]);
+        }
+    }
     return (
         <div className="storyMainContainer flexBox">
-            {posts.map((post)=><img className="imageMainContainer" src={post.image} />)}
+            {imagePosts.map((imgPos)=>(<div key={imgPos.id} style={{marginBottom: "0", maxWidth:"130px", maxHeight: "170px"}}>
+                <img className="imageMainContainer" src={imgPos.image} alt="" />
+                <p className="pMainTag">Soumyadri Das</p>
+            </div>))}
         </div>
     )
 }
