b7a3031a5d1ff5dc901311081f549efce05d6aad
Soumyadri Das <dadsoumyadri@gmail.com>
2021-06-04 12:09:01 +0530

    data filtering and story algorithm executed

--- src/Components/Story/StoryContainer.jsx
+++ src/Components/Story/StoryContainer.jsx
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../Styles/Story/Story.css";
 import "../../App.css";
 import { useSelector } from 'react-redux';
+import filterFriends from '../../Utils/filterFriends';
 
 function StoryContainer(){
     const posts = useSelector(store=>store.posts.posts);
-    console.log(posts)
-    // useEffect(()=>{
-    //     console.log("P",posts)
-    // },[posts])
+    const {user, friends} = useSelector(store=>store.auth)
+    const {users} = useSelector( state => state.app );
+    const [peopleSuggested, setPeopleSuggested] = useState([]);
+    console.log("p",peopleSuggested);
+    const [updated, setUpdated] = useState([]);
+
+    useEffect(() => {
+        setPeopleSuggested( filterFriends( users, friends ) )  
+        //console.log("U", user)
+    }, [users])
+
     var imagePosts = [];
-    for(var i=0; i<posts.length; i++){
+    for(var i=0, j=0; i<posts.length && j<5; i++){
         if(posts[i].hasOwnProperty('image')){
             imagePosts.push(posts[i]);
+            j++;
         }
     }
     return (
         <div className="storyMainContainer flexBox">
-            {imagePosts.map((imgPos)=>(<div key={imgPos.id} style={{marginBottom: "0", maxWidth:"130px", maxHeight: "170px"}}>
-                <img className="imageMainContainer" src={imgPos.image} alt="" />
-                <p className="pMainTag">Soumyadri Das</p>
+            {imagePosts.map((imgPos)=>(<div style={{backgroundImage: `url("${ imgPos.image }")`}} className="mainImgContainer" key={imgPos.id}>
+                {/* <img className="imageMainContainer" src={imgPos.image} alt="" /> */}
+                {peopleSuggested.forEach(element => {
+                    if(element.uid==imgPos.author){
+                        console.log(element.first_name+" "+element.last_name)
+                    }
+                })}
             </div>))}
         </div>
     )
