e632fc0b130f5e9a665499f91033add4068346a8
Soumyadri Das <dadsoumyadri@gmail.com>
2021-06-04 16:56:12 +0530

    StoryBox image container created

--- src/Components/Story/StoryContainer.jsx
+++ src/Components/Story/StoryContainer.jsx
@@ -3,37 +3,14 @@ import "../../Styles/Story/Story.css";
 import "../../App.css";
 import { useSelector } from 'react-redux';
 import filterFriends from '../../Utils/filterFriends';
+import StoryBox from './StoryBox';
 
 function StoryContainer(){
     const posts = useSelector(store=>store.posts.posts);
-    const {user, friends} = useSelector(store=>store.auth)
-    const {users} = useSelector( state => state.app );
-    const [peopleSuggested, setPeopleSuggested] = useState([]);
-    console.log("p",peopleSuggested);
-    const [updated, setUpdated] = useState([]);
 
-    useEffect(() => {
-        setPeopleSuggested( filterFriends( users, friends ) )  
-        //console.log("U", user)
-    }, [users])
-
-    var imagePosts = [];
-    for(var i=0, j=0; i<posts.length && j<5; i++){
-        if(posts[i].hasOwnProperty('image')){
-            imagePosts.push(posts[i]);
-            j++;
-        }
-    }
     return (
         <div className="storyMainContainer flexBox">
-            {imagePosts.map((imgPos)=>(<div style={{backgroundImage: `url("${ imgPos.image }")`}} className="mainImgContainer" key={imgPos.id}>
-                {/* <img className="imageMainContainer" src={imgPos.image} alt="" /> */}
-                {peopleSuggested.forEach(element => {
-                    if(element.uid==imgPos.author){
-                        console.log(element.first_name+" "+element.last_name)
-                    }
-                })}
-            </div>))}
+            {posts.filter((el)=>el.image).filter((el,i)=>i<5).map((post)=><StoryBox key={post.id} {...post} />)}
         </div>
     )
 }
