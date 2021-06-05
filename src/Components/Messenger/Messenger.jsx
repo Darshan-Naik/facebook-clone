e3a02ebba12678180f256d93779906a22e53cd1f
darshan <darshu8314@gmail.com>
2021-06-05 09:58:29 +0530

    chat stystem bug fixed

--- src/Components/Messenger/Messenger.jsx
+++ src/Components/Messenger/Messenger.jsx
@@ -3,21 +3,20 @@ import ChatRoomBody from './ChatRoomBody'
 import ChatRoomList from './ChatRoomList'
 import UserDetails from './UserDetails'
 import "../../Styles/Messenger/Messenger.css"
-import { useSelector } from 'react-redux'
+
 function Messenger({handleRefresh}) {
     const [userDetailsVisibility,setUserDetailsVisibility ] = React.useState(true)
     React.useEffect(handleRefresh)
 
-    const chats = useSelector(store=>store.app.activeContacts)
 
     const handleUserDetailsVisibility=()=>{
         setUserDetailsVisibility(!userDetailsVisibility)
     }
     return (
         <div className="messengerMainContainer flexBox">
-            <ChatRoomList />
-            <ChatRoomBody handleUserDetailsVisibility={handleUserDetailsVisibility} data={chats[0]}/>
-           {userDetailsVisibility && <UserDetails {...chats[0]} />}
+            {/* <ChatRoomList /> */}
+            <ChatRoomBody handleUserDetailsVisibility={handleUserDetailsVisibility} />
+           {userDetailsVisibility && <UserDetails />}
         </div>
     )
 }
