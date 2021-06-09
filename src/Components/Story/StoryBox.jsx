18bdc7d067578991f1c006d52b72cae728d7115e
Soumyadri Das <dadsoumyadri@gmail.com>
2021-06-09 18:38:37 +0530

    Skeleton design & delay problem fixed

--- src/Components/Story/StoryBox.jsx
+++ src/Components/Story/StoryBox.jsx
@@ -6,19 +6,13 @@ import StoryContainerSkeleton from './StorySkeleton/StoryContainerSkeleton';
 function StoryBox({image, author}){
     const [userDetails, setUserDetails] = useState({});
     const users = useSelector(state=>state.app.users);
-    
-    const [load, setLoad] = useState(false);
-    setTimeout(() => {
-        setLoad(true)
-    }, 2500);
 
     useEffect(()=>{
         const user = users.filter((el)=>el.uid===author)
-        //console.log("o",users, author, user)
         setUserDetails(user[0])
     },[users])
 
-    return load?(
+    return userDetails?(
         <div style={{backgroundImage: `url("${image}")`}} className="mainImgContainer">
             <img className="imageMainContainer" src={userDetails?.profilePic || process.env.PUBLIC_URL + '/Images/userProfile_icon.png' } alt="" />
             <p className="pMainTag">{userDetails?.first_name+" "+userDetails?.last_name}</p>
--- src/Components/Story/StorySkeleton/StoryContainerSkeleton.jsx
+++ src/Components/Story/StorySkeleton/StoryContainerSkeleton.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 function StoryContainerSkeleton() {
     const dark = useSelector(store=>store.theme.dark)
     return (
-            <SkeletonTheme width={120} color={dark?"#202020" :"#dadada" } highlightColor={dark?"#444":"#f3efef"} > 
+            <SkeletonTheme width={130} color={dark?"#202020" :"#dadada" } highlightColor={dark?"#444":"#f3efef"} > 
                 <div className="flexBox">
                     <Skeleton style={{borderRadius:"20px", margin: "10px 12px 10px 12px"}} width={110} height={170}/>
                 </div>
