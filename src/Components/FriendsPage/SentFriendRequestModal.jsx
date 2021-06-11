8b9bd568add2c2bbfe985e4b1a21a7a6bcbdeb29
Guru-Eco <abhimanyu768692@gmail.com>
2021-06-11 21:42:29 +0530

    friends page UI and UX updated

---
+++ src/Components/FriendsPage/SentFriendRequestModal.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { ReactComponent as CloseIcon } from "../../Icons/close.svg";
+import SenderFriendCard from "./SenderFriendCard";
+
+function SentFriendRequestModal({ sentRequests, handleSentRequestVisibility }) {
+
+    return (
+        <div className="seeAllSentRequestModalContainer">
+            <div className="seeAllSentRequestModalBox">
+                <div className="flexBox seeAllSentRequestModalHeader">
+                    <h1 className="seeAllSentRequestModalHeaderNamePlate">Sent Requests</h1>
+                    <div className="flexBox seeAllSentRequestModalCloseIcon" onClick={handleSentRequestVisibility}>
+                        <CloseIcon />
+                    </div>
+                </div>
+                <div className="allSentRequestMainContainer scroll">
+                    <div className="allSentRequestModalCount">{`${sentRequests.length} ${ sentRequests.length > 1 ? `Sent Requests` : `Sent Request` }`}</div>
+                    <div>
+                        {
+                            sentRequests?.map( el => <SenderFriendCard key={el.receiverId} {...el} /> )
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SentFriendRequestModal
