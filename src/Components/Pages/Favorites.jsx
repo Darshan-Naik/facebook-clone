952f9a11738db3ed34912ca27190a1967b92d80b
darshan <darshu8314@gmail.com>
2021-06-15 21:58:50 +0530

    video and fav tab style added

--- src/Components/Pages/Favorites.jsx
+++ src/Components/Pages/Favorites.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import FavoritesCard from './FavoritesCard'; 
-
+import SideBar from '../SideBar/SideBar';
 function Favorites({handleRefresh}) {
     const [activePostId,setActivePostId]=React.useState(null);
     const favorites = useSelector(store=>store.auth.favorites)
     React.useEffect(handleRefresh,[]);
-    return favorites.length? (
-        <div className="videoPostContainer scroll">
-            {favorites.map(el=><FavoritesCard key={el.postId} activePostId={activePostId} setActivePostId={setActivePostId} {...el}/>)}
-            
-        </div>
-    ):(
+    return (
+        <div className="videoMainContainer flexBox">
+                <div className="mainLeftSidebarContainer">
+                    <SideBar />
+                </div>         
+              
+            {    favorites.length? (   <div className="videoPostContainer scroll">
+                {favorites.map(el=><FavoritesCard key={el.postId} activePostId={activePostId} setActivePostId={setActivePostId} {...el}/>)}
+                </div>):(
         <div className="pageNotFound flexBox">
             <img src={process.env.PUBLIC_URL + '/Images/sadFace.png'} alt="icon" />
             <h1>You don't have any favorite posts.</h1>
 
         </div>
+    )}
+            </div>
     )
 }
 
